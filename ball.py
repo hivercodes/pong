d159d7416da04fc26d7749caf96eaeb6cebf6ac4
hivercodes <lars.eriksson.mail@gmail.com>
2022-02-12 15:09:43 +0100

    Added movement to ball. Ball now bounces of walls.

--- ball.py
+++ ball.py
@@ -9,4 +9,22 @@ class Ball(Turtle):
         self.shape("circle")
         self.penup()
         self.setheading(45)
+        self.going_up = True
 
+
+    def move(self):
+
+        new_x = self.xcor() + 5
+        if self.ycor() == 280:
+            self.going_up = False
+        if self.ycor() == -280:
+            self.going_up = True
+
+        if self.going_up:
+            new_y = self.ycor() + 5
+        else:
+            new_y = self.ycor() - 5
+            
+
+
+        self.goto(new_x, new_y)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from paddle import Paddle
 from turtle import Screen
 from ball import Ball
+import time
 
 #Setting up the screen size and colourr
 screen = Screen()
@@ -9,23 +10,31 @@ screen.bgcolor("black")
 screen.title("Pyong")
 game_is_on = True
 
+
+#stops screen auto update
 screen.tracer(0)
 
+
+#creates paddles and balls
 paddle_left = Paddle((-350, 0))
 paddle_right = Paddle((350, 0))
 ball = Ball()
 
+#add listeners for keypresses
+screen.listen()
+screen.onkey(paddle_right.paddle_up, "Up")
+screen.onkey(paddle_right.paddle_down, "Down")
+screen.onkey(paddle_left.paddle_up, "w")
+screen.onkey(paddle_left.paddle_down, "s")
 
 
 
-
+#main loop
 while game_is_on:
+    time.sleep(0.1)
+    ball.move()
     screen.update()
-    screen.listen()
-    screen.onkey(paddle_right.paddle_up, "Up")
-    screen.onkey(paddle_right.paddle_down, "Down")
-    screen.onkey(paddle_left.paddle_up, "w")
-    screen.onkey(paddle_left.paddle_down, "s")
+
 
 
 
