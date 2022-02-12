f222fab459c0858e0439fa3d4e2a984e51e55a40
hivercodes <lars.eriksson.mail@gmail.com>
2022-02-12 14:38:44 +0100

    Refractored paddles into their own class

---
+++ main.py
@@ -0,0 +1,54 @@
+from paddle import Paddle
+from turtle import Screen
+
+
+#Setting up the screen size and colourr
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pyong")
+game_is_on = True
+
+screen.tracer(0)
+
+paddle_left = Paddle((-350, 0))
+paddle_right = Paddle((350, 0))
+
+
+
+
+
+
+while game_is_on:
+    screen.update()
+    screen.listen()
+    screen.onkey(paddle_right.paddle_up, "Up")
+    screen.onkey(paddle_right.paddle_down, "Down")
+    screen.onkey(paddle_left.paddle_up, "w")
+    screen.onkey(paddle_left.paddle_down, "s")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+
+
+    def __init__(self, cords):
+        super(Paddle, self).__init__()
+        self.color("white")
+        self.shape("square")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(cords)
+
+    def paddle_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def paddle_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
