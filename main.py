27991606e3d4bd7a99bed83e196cf733e45ab075
hivercodes <lars.eriksson.mail@gmail.com>
2022-02-12 15:28:31 +0100

    Improved ball bounce detection on paddles

--- main.py
+++ main.py
@@ -31,14 +31,17 @@ screen.onkey(paddle_left.paddle_down, "s")
 
 #main loop
 while game_is_on:
+    screen.update()
     time.sleep(0.1)
     ball.move()
-    screen.update()
+    #checks collision with walls
     if ball.ycor() >= 280 or ball.ycor() <= -280:
         ball.bounce()
-    if ball.distance(paddle_right) < 50:
+    #checks collsision with right paddle
+    if ball.distance(paddle_right) < 40 and ball.xcor() > 340:
         ball.ball_return()
-    if ball.distance(paddle_left) < 50:
+    #checks collsision with left paddle
+    if ball.distance(paddle_left) < 40 and ball.xcor() < 340:
         ball.ball_return()
 
 
